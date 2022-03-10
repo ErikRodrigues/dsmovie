2488317b59c470dfe5c5ad0785122d2d243e7834
ErikRodrigues <erickviips16@gmail.com>
2022-03-10 17:15:09 -0300

    Find Movies Part 01

---
+++ backend/src/main/java/com/devsupeior/dsmovie/repositories/MovieRepository.java
@@ -0,0 +1,12 @@
+package com.devsupeior.dsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsupeior.dsmovie.entities.Movie;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+	
+	
+	
+}
